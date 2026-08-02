08572322aa03b25306402e52f4ce622f01228fd0
Moulesh T <91364446+moulesh-t@users.noreply.github.com>
2026-08-02 20:43:29 +0530

    feat: Add Dataset class

--- src/data.py
+++ src/data.py
@@ -1,5 +1,5 @@
 import torch
-
+from torch.utils.data import Dataset
 
 class Vocabulary:
     """
@@ -75,4 +75,61 @@ class Vocabulary:
                 continue
             words.append(word)
         return " ".join(words)
+        
+        
+class TranslationDataset(Dataset):
+    """
+    Pytorch dataset for the AI Translator Model.
+    """
+    
+    def __init__(self, pairs: list[tuple[str,str]], src_vocab: Vocabulary, tgt_vocab: Vocabulary, max_len: int=50) -> None:
+        """
+        Initializes the custom TranslationDataset class using torch Dataset class.
+        
+        Args:
+            pairs (list[tuple[str,str]]): List of (english_sentences, german_sentences) tuples.
+            src_vocab (Vocabulary): Vocabulary object of source language.
+            tgt_vocab (Vocabulary): Vocabulary object of target language.
+            max_len (int): Maximum length of sentence, and defaults to 50 unless specified.
+            
+        Returns:
+            None
+        """
+        self.pairs = pairs
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
+        self.max_len = max_len
+    
+    def __len__(self):
+        """
+        Returns the total number of sentence pairs in the dataset.
+        
+        Returns:
+            int: Number of translation pairs.
+        """
+        return len(self.pairs)
+    
+    def __getitem__(self, index: int) -> tuple[torch.LongTensor, torch.LongTensor]:
+        """
+        Retrieve and process a single translation example.
+        
+        Args:
+            index (int): Starting index of the translation pairs.
+            
+        Returns:
+            tuple[torch.LongTensor, torch.LongTensor]
+        """
+        src_sentence, tgt_sentence = self.pairs[index]
+        src_ids = self.src_vocab.encode(src_sentence)
+        tgt_ids = [self.tgt_vocab.word2idx['<sos>']] + self.tgt_vocab.encode(tgt_sentence) + [self.tgt_vocab.word2idx['<eos>']]
+        if len(src_ids) < self.max_len:
+            src_ids.extend([self.src_vocab.word2idx.get("<pad>", 0)] * (self.max_len - len(src_ids)))
+        else:
+            src_ids = src_ids[:self.max_len]
+        if len(tgt_ids) < self.max_len:
+            tgt_ids.extend([self.tgt_vocab.word2idx.get("<pad>", 0)] * (self.max_len - len(tgt_ids)))
+        else:
+            tgt_ids = tgt_ids[:self.max_len]
+            
+        return (torch.LongTensor(src_ids), torch.LongTensor(tgt_ids))
         
